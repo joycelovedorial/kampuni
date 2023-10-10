ee534c07f1ec444bb7035a03db019b69b990d575
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-10 11:48:53 +0800

    Profile pages

---
+++ src/components/ProfileEdit.vue
@@ -0,0 +1,26 @@
+<template>
+        <h2>Profile</h2>
+        <form>
+            <input type="email" required placeholder="Email" v-model="email">
+            <input type="password" required placeholder="password" v-model="password">
+            <button>Login</button>
+        </form>
+    </template>
+    
+    <script>
+    export default {
+        setup(){
+            const displayName=''
+            const email =''
+            const password = ''
+    
+    
+            return {displayName,email,password}
+        }
+    }
+    </script>
+    
+    <style>
+    
+    </style>
+    
---
+++ src/views/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Logo Container</div>
+        <p>Profile</p>
+        <div>
+            <ProfileEdit/>
+        </div>
+
+        
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+import SignupForm from '@/components/SignupForm.vue';
+export default {
+    components: { LoginForm, SignupForm}
+}
+</script>
+
+<style>
+
+</style>
