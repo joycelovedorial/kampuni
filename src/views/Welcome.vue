c0e5fbddc53438de6688de213b467426b720fad7
Etherea7 <ejkoh2001@gmail.com>
2023-10-10 11:07:38 +0800

    Added REAL files

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Logo Container</div>
+        <p>Welcome</p>
+        <div>Login</div>
+        LoginForm
+        <div>Signup</div>
+        SignupForm
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
