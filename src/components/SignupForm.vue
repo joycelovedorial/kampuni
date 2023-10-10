998626b108244c03b2d342dd82d52177bef52318
Etherea7 <ejkoh2001@gmail.com>
2023-10-10 11:13:02 +0800

    Editted Login/Signup forms

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <form>
+        <input type="text" value="Display Name" v-model="displayName">
+        <input type="email" value="Email" v-model="email">
+        <input type="password" value="password" v-model="password">
+        <button>Sign Up</button>
+    </form>
+</template>
+
+<script>
+export default {
+    setup(){
+        const displayName=''
+        const email =''
+        const password = ''
+
+
+        return {displayName,email,password}
+    }
+}
+</script>
+
+<style>
+
+</style>
