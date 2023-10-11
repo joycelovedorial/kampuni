3d010a9556f0db848b612d35c07fa910117dacc6
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-11 15:40:43 +0800

    Edited login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,13 @@
 <template>
-  <h2>Login</h2>
+    <img src="kampuni_logo.jpg"/> 
     <form>
         <input type="email" required placeholder="Email" v-model="email">
+        <br><br>
         <input type="password" required placeholder="password" v-model="password">
+        <br><br>
         <button>Login</button>
+        <p>or</p>
+        <button>Sign Up</button>
     </form>
 </template>
 
