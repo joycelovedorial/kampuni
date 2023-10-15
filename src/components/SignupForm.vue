f926e16309a1618c47fe22ffc90b17f64edde6f5
Etherea7 <ejkoh2001@gmail.com>
2023-10-16 02:06:01 +0800

    Update SignupForm.vue

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,13 @@
 <template>
     <h2>Signup</h2>
     <form @submit.prevent="handleSubmit">
-        <input type="text" required placeholder="display name" v-model ="displayName">
+        <input type="text" required placeholder="First Name" v-model ="firstName">
+        <input type="text" required placeholder="Last Name" v-model ="lastName">
         <input type="email" required placeholder="email" v-model="email">
         <input type="password" required placeholder='password' v-model="password">
+        <input type="date" id="birthday" v-model="birthday">
+        <input type="text" v-model="country" placeholder="country" id="country">
+        <textarea id="bio" v-model="bio" cols="30" rows="10" placeholder="bio"></textarea>
         <div class="error">{{ error }}</div>
         <button>Sign Up</button>
         <button @click="signupGoogle">Google</button>
@@ -20,14 +24,18 @@ import { auth } from '@/firebase/config'
 export default {
     setup(props,context){
         
-        const displayName = ref("")
+        const firstName = ref("")
+        const lastName = ref("")
+        const birthday = ref("")
+        const country = ref("")
+        const bio = ref("")
         const email = ref("")
         const password = ref("")
 
         const {error, signup} = useSignup()
 
         const handleSubmit = async () => {
-            await signup(email.value, password.value, displayName.value)
+            await signup(email.value, password.value, firstName.value)
             if (!error.value){
               context.emit('signup')
             }
@@ -38,7 +46,7 @@ export default {
             return signInWithPopup(auth, provider);
             };
 
-        return { displayName, email, password, handleSubmit, error, signupGoogle}
+        return { firstName,lastName,country,birthday,bio, email, password, handleSubmit, error, signupGoogle}
     }    
 }
 
