df2aceec046877600f5d778371c289869173d015
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-16 13:08:33 +0800

    Update SignupForm.vue

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -60,11 +60,39 @@ export default {
             if (this.password1 != this.password2){
                 this.error_msg = "passwords do not match";
             }
+            else{
+                addDoc(collection(db, "users"), {
+                    email: this.email,
+                    password: this.password, 
+                })
+            }
             
         }
     }    
 }
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB31YYfSk-z2dIVU-_8fYXuWf4MlviXtdc",
+    authDomain: "kampuni-85a88.firebaseapp.com",
+    projectId: "kampuni-85a88",
+    storageBucket: "kampuni-85a88.appspot.com",
+    messagingSenderId: "983837246823",
+    appId: "1:983837246823:web:67afb33c5b6a5d80751bc2",
+    measurementId: "G-JEKE4M40CN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = firebase(app);
     
 
 </script>
