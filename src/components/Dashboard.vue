09fa1f6d4ac8f7cd10c914a7fa0801fc0549e640
Etherea7 <ejkoh2001@gmail.com>
2023-10-17 10:45:40 +0800

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,9 +2,6 @@
   <p>Components Here: Leaderboard, Calendar</p>
 </template>
 
-
-
-
 <script>
 import Tasklist from './dashboardItems/Tasklist.vue';
 import OutingsCarousel from './dashboardItems/OutingsCarousel.vue';
@@ -12,7 +9,6 @@ import ChatDashboard from './dashboardItems/ChatDashboard.vue';
 import ExpensesList from './dashboardItems/ExpensesList.vue';
 export default {
   components: {Calendar, Leaderboard, Tasklist, ExpensesList, OutingsCarousel, ChatDashboard}
-
 }
 </script>
 
