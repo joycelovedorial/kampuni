fd94f25f6069f76bd50c59c3f60d6e4284515e61
Amoussee <114078162+Amoussee@users.noreply.github.com>
2023-10-17 12:08:15 +0800

    Update Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-fixed" style="background-image: url('img/background1.PNG')"></div>
+    <div class="bg-fixed" style="background-image: url('background_img/background1.PNG')"></div>
     <div>Logo Container</div>
         <p>Welcome</p>
         <div v-if="registered">
