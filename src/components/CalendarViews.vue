e3a6f6a780c8a0daac77b734b87f6c02da366c25
an yu <aannyuuleong@gmail.com>
2023-10-19 17:46:56 +0800

    styled calendar view navbar

--- src/components/CalendarViews.vue
+++ src/components/CalendarViews.vue
@@ -1,18 +1,19 @@
 <template>
-        <nav class="align-top justify-center">
-            <router-link to="../views/chatroom/Chatrooms.vue" class="button text-orangep rounded-l-2xl border-1 border-orangep bg-oranges">Chats</router-link>
-            <router-link to="../views/Calendar.vue" class="button text-orangep border-1 border-orangep bg-oranges">Calendar</router-link>
-            <router-link to="../views/Expenses.vue" class="button text-orangep rounded-r-2xl border-1 border-orangep bg-oranges">Expenses</router-link>
-      </nav>
-  </template>
-  
-  <script>
-  </script>
-  
-  <style>
-    ul {
-      list-style-type: none;
-      display: flex;
-    }
-  
-  </style>  
+  <div class="w-auto">
+    <nav class="flex items-center justify-center h-16"> <!-- Adjusted height and centered vertically -->
+      <router-link to="../views/chatroom/Chatrooms.vue" class="button text-orangep rounded-l-2xl border-1 border-orangep bg-oranges">Outings</router-link>
+      <router-link to="../views/Calendar.vue" class="button text-orangep border-1 border-orangep bg-oranges">Calendar</router-link>
+      <router-link to="../views/Expenses.vue" class="button text-orangep rounded-r-2xl border-1 border-orangep bg-oranges">Tasks</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    display: flex;
+  }
+</style>
