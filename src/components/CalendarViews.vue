f533a087c8a35015913063b496ca030ab5547bbc
Etherea7 <ejkoh2001@gmail.com>
2023-10-20 00:27:15 +0800

    Updating router-links

--- src/components/CalendarViews.vue
+++ src/components/CalendarViews.vue
@@ -2,8 +2,8 @@
   <div class="w-auto">
     <nav class="flex items-center justify-center h-16"> <!-- Adjusted height and centered vertically -->
       <router-link to="../views/chatroom/Chatrooms.vue" class="button text-orangep rounded-l-2xl border-1 border-orangep bg-oranges">Outings</router-link>
-      <router-link to="../views/Calendar.vue" class="button text-orangep border-1 border-orangep bg-oranges">Calendar</router-link>
-      <router-link to="../views/Expenses.vue" class="button text-orangep rounded-r-2xl border-1 border-orangep bg-oranges">Tasks</router-link>
+      <router-link to="/Calendar" class="button text-orangep border-1 border-orangep bg-oranges">Calendar</router-link>
+      <router-link to="/Tasklist" class="button text-orangep rounded-r-2xl border-1 border-orangep bg-oranges">Tasks</router-link>
     </nav>
   </div>
 </template>
