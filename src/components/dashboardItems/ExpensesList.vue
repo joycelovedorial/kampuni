9151353811333f0efc1f206390411a528adeefb9
Amoussee <114078162+Amoussee@users.noreply.github.com>
2023-10-21 14:19:19 +0800

    updates to dashboard

--- src/components/dashboardItems/ExpensesList.vue
+++ src/components/dashboardItems/ExpensesList.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="container">
-     <!-- create a container with four distinct rows -->
-      <div class="row">
-        <div class="col-sm-8">
-          <p>IS216 Constultation</p>
-        </div>
-        <div class="col-sm-2">
-          <p>Done</p>
-        </div>
-        <div class="col-sm-2">
-          <p>Edit</p>
-        </div>
-      </div>
-  </div>
+
 
 
    <!-- BS card: End --> 
--- src/components/dashboardItems/TodayTask.vue
+++ src/components/dashboardItems/TodayTask.vue
@@ -1,5 +1,18 @@
 <template>
-  <p>Tasklist</p>
+    <div class="container bg-orangep">
+     <!-- create a container with four distinct rows -->
+      <div class="row">
+        <div class="col-sm-8">
+          <p>IS216 Constultation</p>
+        </div>
+        <div class="col-sm-2">
+          <p>Done</p>
+        </div>
+        <div class="col-sm-2">
+          <p>Edit</p>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
