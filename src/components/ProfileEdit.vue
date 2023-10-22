aa2b20abde963d6dd7f3a4e8af0190b5a0848e0b
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-22 23:07:40 +0800

    Profile (able to edit now)

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -37,7 +37,7 @@ export default {
         const editbirthday = ref("")
         const editbio = ref("")
         const editcommunity = ref('')
-        const edittoEdit = ref(true)
+        const toEdit = ref(true)
         const fetchData = async () => {
             const user = auth.currentUser;
 
@@ -75,7 +75,7 @@ export default {
         });
             return {updateEdit,firstname, lastname, birthday, country, bio, email, community,toEdit,
                     editfirstname,editlastname,editemail,editcountry,editbirthday,editbio,editcommunity,
-          }
+            }
     }
 }
 </script>
