9aafb2964a40f76323377768a84dda712147cdb2
Etherea7 <ejkoh2001@gmail.com>
2023-10-22 23:46:33 +0800

    debugged

--- src/views/joinCommunity.vue
+++ src/views/joinCommunity.vue
@@ -14,20 +14,42 @@
 <script>
 
 import CommunityList from '@/components/joinCommunityItems/CommunityList.vue';
-import { db } from "@/firebase/config"
+import { auth, db } from "@/firebase/config"
 import { ref, onMounted } from 'vue'
 import CommunityCreate from '@/components/joinCommunityItems/CommunityCreate.vue';
+import { useRouter } from 'vue-router';
+import { doc,getDoc } from 'firebase/firestore';
 
 
 export default {
   components: {CommunityList,CommunityCreate},
   setup(){
     const createCom = ref(false)
+    const router = useRouter()
+    const user = auth.currentUser
+    console.log("this is supposed to be user " +  user);
+    
+    const fetchData = async () => {
+      if (user) {
+      console.log(user);
+      const uid = user.uid;
+      const docRef = doc(db,"users",uid)
 
-
-    return { createCom }
-}
-
+      const docSnap = await getDoc(docRef)
+      const userData = docSnap.data()
+      console.log("this is supposed to be user Data" +  userData);
+      if (userData.community) {
+            router.push({ name: 'Homepage' });
+      }
+      }else{
+        router.push({name:"Welcome"})
+      }
+    }
+    onMounted(() => {
+            fetchData(); // Fetch data after the component is mounted
+        });
+return { createCom }
+  }
 }
 
 
