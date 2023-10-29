146a22f5556a5bc7271278df98ff626e295177db
an yu <aannyuuleong@gmail.com>
2023-10-30 00:39:26 +0800

    slightly styled createtask popup!

--- src/components/PlannerItems/Tasklist/createTask.vue
+++ src/components/PlannerItems/Tasklist/createTask.vue
@@ -1,17 +1,66 @@
+<!-- <template>
+    <div class="container create-task bg-white">
+        <div class="fixed inset-0 bg-black opacity-50" v-if="showPopup" @click="closePopup">
+            <transition name="popup">
+    
+                    <form @submit.prevent>
+                        <div class="d-block">
+                            <label for="taskname">Task Name</label>
+                            <input type="text" id="taskname" v-model="taskname">
+                        </div>
+                
+                        <div class="d-block">
+                            <label for="description">Description</label>
+                            <input type="text" id="description" v-model="description">
+                        </div>
+                
+                        <div class="d-block">
+                            <label for="dateline">Dateline</label>
+                            <input type="datetime-local" id="dateline" v-model="dateline">
+                        </div>
+                
+                        <div class="d-block">
+                            <label for="countdown">Allocate By</label>
+                            <input type="datetime-local" id="countdown" v-model="countdown">
+                        </div>
+                        <button @click="createTask">Create!</button>
+                    </form>
+            </transition>
+        </div>
+        
+    </div>
+  </template>
+   -->
 <template>
-  <div class="container create-task">
-    <form @submit.prevent>
-        <label for="taskname">Task Name</label>
-        <input type="text" id="taskname" v-model="taskname">
-        <label for="descr">Description</label>
-        <input type="text" id="desc" v-model="description">
-        <label for="dateline">Dateline</label>
-        <input type="datetime-local" id="dateline" v-model="dateline">
-        <label for="countdown">Allocate By</label>
-        <input type="datetime-local" id="countdown" v-model="countdown">
-        <button @click="createTask">Create!</button>
-    </form>
-  </div>
+    <transition name="popup">
+        <div class="popup create-task bg-white">
+            <h1 class="font-bold leading-loose text-center">
+                New Task
+            </h1>
+            <form @submit.prevent>
+                <div class="d-block">
+                    <label class="text-sm d-block" for="taskname">Task Name</label>
+                    <input type="text" id="taskname" v-model="taskname">
+                </div>
+
+                <div class="d-block">
+                    <label class="text-sm d-block" for="desc">Description</label>
+                    <input type="text" id="desc" v-model="description">
+                </div>
+
+                <div class="d-block">
+                    <label class="text-sm d-block" for="dateline">Dateline</label>
+                    <input type="datetime-local" id="dateline" v-model="dateline">
+                </div>
+
+                <div class="d-block">
+                    <label class="text-sm d-block" for="countdown">Allocate By</label>
+                    <input type="datetime-local" id="countdown" v-model="countdown">
+                </div>
+                <button @click="createTask">Create!</button>
+            </form>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -70,6 +119,32 @@ export default {
 </script>
 
 <style>
+/* Style for pop-up container */
+.popup {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+}
 
-
+/* Vue transition for the floating down effect */
+.popup-enter-active {
+  transition: transform 5s ease-in-out;
+}
+.popup-enter-from {
+  transform: translate(-50%, -70%);
+}
+.popup-enter-to {
+  transform: translate(-50%, -50%);
+}
+.popup-leave-active {
+  transition: transform 5s ease-in-out;
+}
+.popup-leave-to {
+  transform: translate(-50%, -70%);
+}
 </style>
