9dc0010f0abda1b43fe9295fdf79f2e77edbbe7a
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-31 00:58:02 +0800

    trying to get events

--- src/components/PlannerItems/Schedule/Calendar.vue
+++ src/components/PlannerItems/Schedule/Calendar.vue
@@ -1,5 +1,6 @@
 <template>
   <div>My Calendar</div>
+  
 </template>
 
 <script setup>
@@ -9,7 +10,24 @@ import { getAuth, signInWithRedirect,getRedirectResult, GoogleAuthProvider } fro
 import { useTokenStore } from '../../../store/store.js'
 
 const store = useTokenStore()
-console.log(store.token,store.useremail)
+console.log(store.token, store.useremail)
+
+/*
+axios.get("https://www.googleapis.com/calendar/v3/calendars/calendarId/events", {
+  params: {
+    calendarId: store.token,
+  }
+})
+.then(response => {
+  var my_events = response.data();
+  for (a_event in my_events){
+    console.log(a_event);
+  }
+})
+.catch(error => {
+  console.log(error.message)
+})
+*/
 
 </script>
 
