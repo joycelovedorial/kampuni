b1f8e29a99fa533ecffabad4ceea6a7e28661749
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-31 11:47:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "tailwindcss/tailwind.css"
-
+import { createPinia } from 'pinia'
 import { auth } from '@/firebase/config'
+
+const pinia = createPinia()
 
 let app
 auth.onAuthStateChanged(()=> {
     if(!app){
         app = createApp(App)
         .use(router)
+        .use(pinia)
         .mount('#app')
     }
 })
+
+
+
