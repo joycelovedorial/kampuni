4c508897cd7fcbc70cc0bc27acad3b9d7acb4818
Etherea7 <ejkoh2001@gmail.com>
2023-10-31 11:56:27 +0800

    a

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "tailwindcss/tailwind.css"
-
 import { auth } from '@/firebase/config'
+import { createPinia } from 'pinia'; // Import createPinia
+const pinia = createPinia();
 
 let app
 auth.onAuthStateChanged(()=> {
     if(!app){
         app = createApp(App)
         .use(router)
+        .use(pinia)
         .mount('#app')
     }
 })
