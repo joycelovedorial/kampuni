ce4981e17c38cf5cf6cbc87bf7bff55b6596839a
SandraTan08 <114910062+SandraTan08@users.noreply.github.com>
2023-10-31 15:39:37 +0800

    PUSH RIGHT NOW

--- src/components/dashboardItems/Expenses.vue
+++ src/components/dashboardItems/Expenses.vue
@@ -6,32 +6,33 @@
       <div class="row">
         <div>
           <label for="name">Name of Person who paid</label>
-          <input class="form-control col-6" type="text" v-model="name" id="name">
+          <input class="form-control col-6" type="text" v-model="person_paid" id="name">
         </div>
 
         <div>
           <label for="expense_name">Name of Expense</label>
-          <input class="form-control col-6" type="text" v-model="exepense_name" id="expense_name">
+          <input class="form-control col-6" type="text" v-model="expense_desc" id="expense_name">
         </div>
 
         <div>
           <label for="expense_cost">Cost of Expense</label>
-          <input class="form-control col-6" type="number" v-model="expense_cost" id="expense_cost">
+          <input class="form-control col-6" type="number" v-model="cost" id="expense_cost">
         </div>
 
         <div>
           <label for="expense_category">Category of Expense</label>
-          <select>
+          <select v-model="category">
             <option>Outing Expense</option>
             <option selected>Non Outing Expense </option>
           </select>
         </div>
 
         <!--END of input form for expenses-->
+        {{ expense_desc }}
 
         <!--START of button to add more expenses-->
         <div>
-          <button @click="add_expense" type="submit">Add Expense</button>
+          <button @click="addExpense" type="submit">Add Expense</button>
         </div>
         <!--END of button to add more expenses-->
 
@@ -48,80 +49,72 @@
 
 <script>
 
-import { ref } from 'vue';
-import { auth, db, addDoc, collection, getDoc, doc } from '@/firebase/config';
+import { ref,onMounted } from 'vue';
+import { auth, db} from '@/firebase/config';
+import { addDoc, collection, getDoc, doc } from "firebase/firestore";
 
 export default {
-  data(){
-    const sum = 0;
-    
-  
-  },
-
   setup() {
     //this is a Vue.js 3 composition API function 
     //where you define reactive data and functions 
     
     //reactive references with initial values as empty string 
     //which are used for form inputs in the template
-    const name = ref(''); 
-    const amount = ref('');
-    const category = ref('');
+    const person_paid = ref(''); 
+    const expense_desc = ref('');
+    const cost = ref('');
+    const category = ref("")
+    const comid = ref("")
+    const userid = ref("")
 
     /*creating an addExpenses asynchronous function 
     asynchronous function is a function that performs a specific task in a non-blocking manner
     it does not block or wait for the result from the previous line of code to execute
     */
+   
     const addExpense = async () => {
-      const user = auth.currentUser; 
-      const uid = user.uid; //retrieves the current authenticated user and their unique ID
-      //fetches the user's document from a 'users' collection in a database
-      const docSnap = await getDoc(doc(db, 'users', uid)); 
-      const docData = docSnap.data();
-      const cid = docData.community; //retrieve the community ID from the user's document
-
-      /*creates an expense object with the values from the form inputs*/
-        const expense = {
-        name: expense_name.value,
-        amount: parseFloat(expense_cost.value),
-        category: expense_category.value,
-      };
-
-      await addDoc(collection(db, 'expenses'), {
-        /*collection(db, `expenses`) is calling the expense collection function from firebase firestore
-        adding a new document to the specified expenses collection
+      try{
+        const oppar = await addDoc(collection(db, 'expenses'), {
+          name: person_paid.value,
+          desc: expense_desc.value,
+          amount: cost.value,
+          category: category.value, 
+        });
+        if (oppar){
+          console.log("doc added")
+        }
+        /*code is resetting the values of the form inputs to empty strings
+        it helps to clear the form inputs for the next entry providing a better user experience
+        after the addExpense function is executed successfully and sent to the database 
+        it will reset the name, amount, and category values to empty
         */
+      } catch (error){
+      console.log(error.message)
+    }
+  };
+    const fetchData = async () =>{
 
-        /*
-        The object passed as the second argument to the addDoc 
-        function is the data to be added to the document
-        */
+      const user = auth.currentUser
+      userid.value = user.uid
+      const docRef = doc(db,"users",userid.value)
+      const docSnap = await getDoc(docRef)
+      const docData = docSnap.data()
+      comid.value = docData.community
+    }
 
-  
-        name: expense.expense_name, //adding a field named 'name' with the value of expense.name
-        amount: expense.expense_cost, //adding a field named 'amount' with the value of expense.amount
-        category: expense.expense_category, //adding a field named 'category' with the value of expense.category
-      });
+    onMounted(()=>{
+      fetchData()
+    })
 
-      /*code is resetting the values of the form inputs to empty strings
-      it helps to clear the form inputs for the next entry providing a better user experience
-      after the addExpense function is executed successfully and sent to the database 
-      it will reset the name, amount, and category values to empty
-      */
-      name.value = '';
-      amount.value = 0;
-      category.value = '';
-    };
-
+    
     return {
-      name,
-      amount,
-      category,
-      addExpense,
+      addExpense, person_paid, expense_desc, cost, category
 
     };
-  },
-};
+  }
+}
+
+
 </script>
 
 <style>
