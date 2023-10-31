839cf7fdebd73ab4bf6c11c28c795b6b0e43c5be
joycelovedorial <gileaticsay.2020@scis.smu.edu.sg>
2023-10-31 11:18:32 +0800

    Expense Split
    Expense Split

--- src/components/dashboardItems/Expenses.vue
+++ src/components/dashboardItems/Expenses.vue
@@ -1,17 +1,87 @@
 <template>
-  Expenses
+  <div class="create-expenses container-fluid">
+    <form @submit.prevent="addExpense">
+      <h2>Expenses</h2>
+      <div class="row">
+        <div>
+          <label for="name">Name</label>
+          <input class="form-control col-6" type="text" v-model="name" id="name">
+        </div>
+
+        <div>
+          <label for="amount">Amount</label>
+          <input class="form-control col-6" type="number" v-model="amount" id="amount">
+        </div>
+
+        <div>
+          <label for="category">Category</label>
+          <input class="form-control col-6" type="text" v-model="category" id="category">
+        </div>
+
+        <div>
+          <button type="submit">Add Expense</button>
+        </div>
+
+        <div>
+          <button type="submit">Calculate</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import ExpensesList from './ExpensesList.vue';
-import Leaderboard from './Leaderboard.vue';
+import { ref } from 'vue';
+import { auth, db, addDoc, collection, getDoc, doc } from '@/firebase/config';
+
 export default {
-    components: {
-        ExpensesList, Leaderboard
-    }
-}
+  data(){
+    const sum = 0;
+    
+  
+  },
+
+
+  setup() {
+    const name = ref('');
+    const amount = ref('');
+    const category = ref('');
+
+    const addExpense = async () => {
+      const user = auth.currentUser;
+      const uid = user.uid;
+      const docSnap = await getDoc(doc(db, 'users', uid));
+      const docData = docSnap.data();
+      const cid = docData.community;
+
+      const expense = {
+        name: name.value,
+        amount: parseFloat(amount.value),
+        category: category.value,
+      };
+
+      await addDoc(collection(db, 'expenses'), {
+        community: cid,
+        name: expense.name,
+        amount: expense.amount,
+        category: expense.category,
+      });
+
+      name.value = '';
+      amount.value = '';
+      category.value = '';
+    };
+
+    return {
+      name,
+      amount,
+      category,
+      addExpense,
+    };
+  },
+};
 </script>
 
 <style>
-
+/* Add any necessary styles here */
 </style>
--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
     <Navbar/>
+  <div id="expenses-page">
+
+  <Expense @add-expense="addExpense" />
+
+
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Expenses from '@/components/dashboardItems/Expenses.vue';
 export default {
-components:{Navbar}
-
+components:{
+  Navbar,
+  Expenses
+},
+setup(){
+  return{}
+}
 }
 </script>
 
 <style>
+#expenses-page {
+  background-image: url('../assets/background_img/background4.png');
+  background-color: #f2f2f2;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+}
 
 </style>
