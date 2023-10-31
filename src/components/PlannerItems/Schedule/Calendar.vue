248e5f0384d1d1940a5e0f012a909a11e6b01ee1
Etherea7 <ejkoh2001@gmail.com>
2023-10-31 13:55:03 +0800

    Update

--- src/components/PlannerItems/Schedule/Calendar.vue
+++ src/components/PlannerItems/Schedule/Calendar.vue
@@ -2,14 +2,45 @@
   <div>My Google Calendar</div>
 </template>
 
-<script setup>
+<script>
+import { ref } from 'vue'
 // import { capitalize } from './helpers'
-import {auth} from "../../../firebase/config.js"
-import { getAuth, signInWithRedirect,getRedirectResult, GoogleAuthProvider } from "firebase/auth";
-import { useTokenStore } from '../../../store/store.js'
+import {db,auth} from "../../../firebase/config.js"
+export default{
+  setup(){
+    const taskArray =ref([])
+    const outingArray = ref([])
+    const comid = ref("")
+    const userid = ref("")
+    
 
-const store = useTokenStore()
-console.log(store.token,store.useremail)
+    const q = query(collection(db,"tasks"),where("userid","==",null))
+        const unsub = onSnapshot(q,(snap)=>{
+            const results= [];
+            snap.forEach((doc)=>{
+                results.push({ ...doc.data(), id: doc.id })
+            })
+            taskArray.value=results
+        })
+
+    
+    const fetchData = async () =>{
+      const user = auth.currentUser
+      userid.value = user.uid
+      const docRef = doc(db,"users",userid.value)
+      const docSnap = await getDoc(docRef)
+      const docData = docSnap.data()
+      comid.value = docData.community
+      
+
+    }
+
+
+    return {
+
+    }
+  }
+}
 
 </script>
 
