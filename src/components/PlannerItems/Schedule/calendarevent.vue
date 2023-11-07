b617180da79ce1c0a51f6bac790d4a90f4e5a467
Etherea7 <ejkoh2001@gmail.com>
2023-11-07 08:27:44 +0800

    a

---
+++ src/components/PlannerItems/Schedule/calendarevent.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="centered-component artboard artboard-horizontal phone-2">
+       <div>
+           Title:{{ title }}
+           Description: {{ description }}
+           Location: {{  location }}
+           Date: {{ date }}
+           Time: {{ time }}
+       </div>
+
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+   props:{
+       eobj:Object,
+   },
+   setup(props,context){
+        const date = ref(null); // To store the date part
+        const time = ref(null); // To store the time part
+        const title= ref(props.eobj.title)
+        const location = ref(props.eobj.location)
+        const description = ref(props.eobj.description)
+
+
+
+        const updateDateAndTime = () => {
+            if (props.oobj && props.oobj.date) {
+                const timestamp = props.oobj.date.seconds;
+                const dateObj = new Date(timestamp);
+                
+        
+                date.value = dateObj.toISOString().split('T')[0];
+                time.value = dateObj.toTimeString().split(' ')[0];
+            } else {
+                date.value = null;
+                time.value = null;
+            }
+        };
+
+
+        updateDateAndTime();
+
+       return{
+           title,location,description,date,time
+       }
+   }
+}
+</script>
+
+<style>
+.centered-component {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
