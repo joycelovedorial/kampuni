0106e4ae2eacc0e15347fcea054a57bd838e2df8
Etherea7 <ejkoh2001@gmail.com>
2023-11-07 23:02:19 +0800

    Update calendarevent.vue

--- src/components/PlannerItems/Schedule/calendarevent.vue
+++ src/components/PlannerItems/Schedule/calendarevent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="centered-component bg-r">
-        <div class="container">
-        <div class= "row">
+        <div class="container" v-if="toEdit">
+        <div class= "row" >
             {{ title }}
             </div>
             <div class= "row">
@@ -16,6 +16,26 @@
             <div class= "row">
             Time: {{ time }}
         </div>
+        <button @click="toEdit=!toEdit">Edit</button>
+    </div>
+    <div class="container" v-else>
+        <div class= "row" >
+            <input type="text" :placeholder="title" v-model="etitle">
+            </div>
+            <div class= "row">
+                <input type="text" :placeholder="description" v-model="edesc">
+            </div>
+            <div class= "row">
+                <input type="text" :placeholder="location" v-model="elocation">
+            </div>
+            <div class= "row">
+                <input type="date" :placeholder="date" v-model="edate">
+            </div>
+            <div class= "row">
+            <input type="time" :placeholder="time" v-model="etime">
+        </div>
+        <button @click="editEvent">Confirm Edit Event</button>
+
     </div>
 
     </div>
@@ -23,6 +43,8 @@
 
 <script>
 import { ref } from 'vue'
+import { db } from '@/firebase/config';
+import { doc, updateDoc } from 'firebase/firestore';
 export default {
    props:{
        eobj:Object,
@@ -33,7 +55,12 @@ export default {
         const title= ref(props.eobj.title)
         const location = ref(props.eobj.location)
         const description = ref(props.eobj.description)
-
+        const toEdit=ref(true)
+        const etitle=ref('')
+        const elocation =ref('')
+        const edesc = ref('')
+        const etime =ref('')
+        const edate=ref('')
 
 
         const updateDateAndTime = () => {
@@ -53,8 +80,26 @@ export default {
 
         updateDateAndTime();
 
+        const editEvent = async()=>{
+            const combinedDateTime = new Date(`${edate.value}T${etime.value}`);
+
+                // Check if combinedDateTime is a valid Date
+                if (isNaN(combinedDateTime)) {
+                // Handle the case where the date and time input are invalid
+                console.error('Invalid date or time input');
+                return;
+                }
+
+            const uref = updateDoc(doc(db,"events",props.eobj.id),{
+                description:edesc.value,
+                title:etitle.value,
+                location:elocation.value,
+                date:combinedDateTime,
+            })
+        }
+
        return{
-           title,location,description,date,time
+           title,location,description,date,time,toEdit,editEvent,etitle,elocation,edesc,edate,etime
        }
    }
 }
